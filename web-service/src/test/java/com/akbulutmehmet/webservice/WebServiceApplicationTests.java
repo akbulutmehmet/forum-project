d20542aab25d24c6edfb98211b7dcae15262ebf4
mehmetakbulut <mehmetakbulut.x@gmail.com>
2023-04-06 16:24:02 +0300

    first commit

---
+++ web-service/src/test/java/com/akbulutmehmet/webservice/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akbulutmehmet.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
