bb62ac2242e9045d6cf686604b19de1f9eada2a2
mehmetakbulut <mehmetakbulut.x@gmail.com>
2023-04-17 04:22:44 +0300

    mapper remove

--- web-service/src/main/java/com/akbulutmehmet/webservice/service/CategoryService.java
+++ web-service/src/main/java/com/akbulutmehmet/webservice/service/CategoryService.java
@@ -5,14 +5,12 @@ import com.akbulutmehmet.webservice.dto.converter.CategoryDtoConverter;
 import com.akbulutmehmet.webservice.dto.request.CreateCategoryRequest;
 import com.akbulutmehmet.webservice.dto.response.CategoryDto;
 import com.akbulutmehmet.webservice.exception.CategoryException;
-import com.akbulutmehmet.webservice.mapper.ICategoryMapper;
 import com.akbulutmehmet.webservice.model.Category;
 import com.akbulutmehmet.webservice.repository.CategoryRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -20,18 +18,20 @@ import java.util.stream.Collectors;
 @Transactional(propagation = Propagation.REQUIRED,readOnly = true)
 public class CategoryService {
     private final CategoryRepository categoryRepository;
-    private final ICategoryMapper categoryMapper;
+
     private final CategoryDtoConverter categoryDtoConverter;
 
-    public CategoryService(CategoryRepository categoryRepository, ICategoryMapper categoryMapper, CategoryDtoConverter categoryDtoConverter) {
+    public CategoryService(CategoryRepository categoryRepository, CategoryDtoConverter categoryDtoConverter) {
         this.categoryRepository = categoryRepository;
-        this.categoryMapper = categoryMapper;
         this.categoryDtoConverter = categoryDtoConverter;
     }
 
     @Transactional(readOnly = false)
     public Category createCategory(CreateCategoryRequest createCategoryRequest) {
-        return categoryRepository.save(categoryMapper.toCategory(createCategoryRequest));
+        Category category = new Category();
+        category.setDescription(createCategoryRequest.getDescription());
+        category.setTitle(createCategoryRequest.getTitle());
+        return categoryRepository.save(category);
     }
 
     public Set<CategoryDto> listCategory() {
