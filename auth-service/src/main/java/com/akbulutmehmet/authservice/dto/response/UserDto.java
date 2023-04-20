2e4ab87fe8cc56980012cd3cea451839d1f22664
mehmetakbulut <mehmetakbulut.x@gmail.com>
2023-04-21 00:54:32 +0300

    spring security update

--- auth-service/src/main/java/com/akbulutmehmet/authservice/dto/response/UserDto.java
+++ auth-service/src/main/java/com/akbulutmehmet/authservice/dto/response/UserDto.java
@@ -1,26 +1,34 @@
 package com.akbulutmehmet.authservice.dto.response;
 
+import com.akbulutmehmet.authservice.model.Role;
+
 import java.io.Serializable;
-import java.util.List;
 
 public class UserDto implements Serializable {
     private String id;
     private String name;
     private String surName;
     private String username;
-
+    private Role role;
 
     public UserDto() {
     }
 
-    public UserDto(String id, String name, String surName, String username) {
+    public UserDto(String id, String name, String surName, String username, Role role) {
         this.id = id;
         this.name = name;
         this.surName = surName;
         this.username = username;
-
+        this.role = role;
     }
 
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
 
     public String getId() {
         return id;
