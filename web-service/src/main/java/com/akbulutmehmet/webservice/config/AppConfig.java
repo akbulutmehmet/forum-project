54e76c75aad0aa89cb6c3de602adb52b0f69e17b
mehmetakbulut <mehmetakbulut.x@gmail.com>
2023-04-21 02:15:53 +0300

    interceptor add

--- web-service/src/main/java/com/akbulutmehmet/webservice/config/AppConfig.java
+++ web-service/src/main/java/com/akbulutmehmet/webservice/config/AppConfig.java
@@ -1,16 +1,14 @@
 package com.akbulutmehmet.webservice.config;
 
 import com.akbulutmehmet.webservice.interceptor.AuthInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 
-public class AppConfig extends WebMvcConfigurerAdapter {
+public class AppConfig implements WebMvcConfigurer {
     private final AuthInterceptor authInterceptor;
 
     public AppConfig(@Lazy  AuthInterceptor authInterceptor) {
