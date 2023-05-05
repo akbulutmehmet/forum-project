16a809628d8b80080b2349545007cd7fe4175285
mehmetakbulut <mehmetakbulut.x@gmail.com>
2023-05-05 17:03:33 +0300

    front end new post page add

--- forum-fe/src/pages/new-post/index.js
+++ forum-fe/src/pages/new-post/index.js
@@ -1,9 +1,30 @@
 import Header from "../../components/header";
 import Footer from "../../components/footer";
 import { useEffect, useState } from "react";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
+import postService from "../../services/PostService";
 const Newpost = () =>  {
     const [post,setPost] = useState({});
+    const navigate = useNavigate();
+    const onChangeTitle = (event) => {
+        setPost({
+            ...post,
+            "title":event.target.value
+        });
+    }
+    const onChangeContent = (event) => {
+        setPost({
+            ...post,
+            "content":event.target.value
+        })
+    }
+    const onClickBtn = () => {
+        postService.createPost("/api3/api/v1/post/createPost",JSON.stringify(post),(response) => {
+            if(response.status === 200) {
+                navigate("/post/"+response.data.id);
+            }
+        });
+    }
     const {categoryId} = useParams();
     useEffect(() => {
         setPost({
@@ -17,14 +38,14 @@ const Newpost = () =>  {
                 <div className="col-md-8">
                     <div className="form-gruop mt-3">
                         <label htmlFor="title">Title</label>
-                        <input className="form-control" id="title" type="text" />
+                        <input className="form-control" id="title" type="text" onChange={onChangeTitle}/>
                     </div>
                     <div className="form-gruop mt-3">
-                        <label htmlFor="description">Description</label>
-                        <input className="form-control" id="description" type="text" />
+                        <label htmlFor="description">Content</label>
+                        <input className="form-control" id="content" type="text" onChange={onChangeContent}/>
                     </div>
                     <div className="form-gruop mt-3">
-                        <button onClick={"onClickBtn"} className="btn btn-lg btn-success">CREATE</button>
+                        <button onClick={onClickBtn} className="btn btn-lg btn-success">CREATE</button>
                     </div>
                 </div>
             </div>
--- forum-fe/src/services/PostService.js
+++ forum-fe/src/services/PostService.js
@@ -1,4 +1,6 @@
+import Swal from "sweetalert2";
 import Getaxios from "../utils/Getaxios";
+import PostaxiosWithAuth from "../utils/PostAxiosWithAuth";
 
 class PostService { 
     getPostById = (url,id,callback) => {
@@ -10,6 +12,26 @@ class PostService {
             console.log(error);
         })
     };
+    createPost = (url,data,callback) => {
+        PostaxiosWithAuth(url,data).then((response) => {
+            if(callback) {
+                callback(response);
+            }
+            Swal.fire({
+                title: "Post create ok",
+                    text:"Post create ok",
+                    icon:"success"
+            })
+        }).catch((error) => {
+            Swal.fire(
+                {
+                    title:error.message,
+                    text:error.message,
+                    icon:"error"
+                }
+            );
+        })
+    }
 }
 const postService = new PostService();
 export default postService;
